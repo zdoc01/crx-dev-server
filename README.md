49015597a1dc047bab337793427edc32a94bab55
Zach <zdoc01@gmail.com>
2019-03-24 20:50:42 -0700

    feat(init): first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# crx-dev-server
+
+A dev "server" for Chrome extensions with hot reload capabilities.
+
+# Installation
+
+```sh
+$ npm i --save-dev crx-dev-server
+```
+
+# Usage
+
+By default, this package will look for a `manifest.json` definition in `process.cwd()`.
+
+```sh
+$ crx-dev-server
+```
+
+If you wish to use a `manifest.json` from a different location, simply specify the path via the `--manifest` command-line option:
+
+```sh
+$ crx-dev-server --manifest=/path/to/your/manifest.json
+```
+
+# How does it work?
+
+This package injects a background script (see [crx-hot-reload]()) into your project's `manifest.json` that watches for file changes and auto-reloads your extension when they occur.
+
+The background script (`hot-reload.js`) is copied into your project alongside your `manifest.json` (Chrome requires content and background scripts be local and relative to your `manifest.json`), then removed when the server is stopped.
+
+# Motivation
+
+I love the work that went into `crx-hot-reload` as it makes my life easier as an extension developer, but I wanted a way to keep the background script definition out of my `manifest.json` so I could ship the extension without dev assets.
+
+You can achieve something similar with a CI/CD pipeline, but that isn't necessarily applicable to every project.
---
+++ index.js
@@ -0,0 +1,88 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+
+const ENCODING = { encoding: 'utf8' };
+const MAX_TIMEOUT_DURATION = 2147483647;
+const HOT_RELOAD_SCRIPT = 'hot-reload.js';
+const args = process.argv[2];
+
+// Use the manifest in the current directory by default,
+// otherwise use the path defined in --manifest
+let extManifest = './manifest.json';
+if (!fs.existsSync(path.resolve(extManifest))) {
+  if (args.length === 3 && args[2].startsWith('--manifest')) {
+    /* eslint-disable-next-line prefer-destructuring */
+    extManifest = args.split('=')[1];
+  } else {
+    console.log('No manifest found.');
+    console.log(
+      'Add one to the current directory, or specify a path via the --manifest option'
+    );
+    console.log('Usage: `crx-dev-server --manifest=/path/to/manifest.json`');
+    process.exit(1);
+  }
+}
+
+const MANIFEST_PATH = path.resolve(extManifest);
+const HOT_RELOAD_PATH = MANIFEST_PATH.replace(
+  'manifest.json',
+  HOT_RELOAD_SCRIPT
+);
+
+if (!fs.existsSync(MANIFEST_PATH)) {
+  console.log(`No manifest found at the specified path ${MANIFEST_PATH}`);
+  process.exit(1);
+}
+
+const original = JSON.parse(
+  fs.readFileSync(MANIFEST_PATH, { encoding: 'utf8' })
+);
+
+const withHotReload = (mf = {}) => {
+  const bg = { ...mf.background };
+  const scripts = [...bg.scripts, HOT_RELOAD_SCRIPT];
+  return {
+    ...mf,
+    background: {
+      ...bg,
+      scripts
+    }
+  };
+};
+
+const writeManifest = data => {
+  fs.writeFileSync(MANIFEST_PATH, JSON.stringify(data, null, 2), ENCODING);
+};
+
+const copyHotReload = () => {
+  fs.copyFileSync(require.resolve('crx-hotreload'), HOT_RELOAD_PATH);
+};
+
+const removeHotReload = () => {
+  fs.unlinkSync(HOT_RELOAD_PATH);
+};
+
+// add hot reload capabilities
+const devManifest = withHotReload(original);
+writeManifest(devManifest);
+// Chrome requires files be local and relative to the
+// `manifest.json`, so we're copying the script that
+// handles hot reload functionality into the project
+// as opposed to serving it over HTTP(s), etc.
+copyHotReload();
+
+// listen for script close
+process.on('SIGINT', () => {
+  // reset back to original manifest
+  writeManifest(original);
+  removeHotReload();
+  process.exit(0);
+});
+
+// keep process alive to emulate "server"
+setInterval(() => {}, MAX_TIMEOUT_DURATION);
+
+console.log('Hot reload for your extension is up and running.');
+console.log('Start making changes and watch the magic happen!');
